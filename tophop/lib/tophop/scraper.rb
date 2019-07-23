d37db14c72b2288018c2b78a8c3f859bbf05cf9a
boruch d <dubywider@gmail.com>
2019-07-23 17:46:55 +0000

    busting it out

--- tophop/lib/tophop/scraper.rb
+++ tophop/lib/tophop/scraper.rb
@@ -1,10 +1,13 @@
 class Scraper
- def get_page
-   Nokogiri::HTML(open("https://www.businessinsider.com/top-us-hospitals-ranked-for-medical-care-by-us-news-2019-6#20-brigham-and-womens-hospital-1"))
+
+ def self.pull_page
+   coin =[]
+  index_page = Nokogiri::HTML(open("https://www.businessinsider.com/top-us-hospitals-ranked-for-medical-care-by-us-news-2019-6"))
+  binding.pry
+ index_page.css(".slide-wrapper").text.strip
+
  end
- def get_hosp
-   self.get_page.css(".slide-wrapper")
-  end
+ 
   # index_page.css(""").each do |hospital|
      #hospital
      #hospital.name = hospital.css("image-caption").text
@@ -15,9 +18,12 @@ class Scraper
     # hospital_link = "#{hospital.attr('href')}"
     # hospital_bio = hospital.css(".class")
     
-      
-   
+       #index_page.css(".image-caption")image-caption.text
+   #index_page.css(".slide-title-text").text.strip
 
 
 
-end  
+  
+  
+end 
+
