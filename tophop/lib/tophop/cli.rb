1f10c6ebb51dd8b4675dc9cb3c722d6037358648
boruch d <dubywider@gmail.com>
2020-10-13 22:51:02 +0000

    Automatically backed up by Learn

--- tophop/lib/tophop/cli.rb
+++ tophop/lib/tophop/cli.rb
@@ -4,10 +4,18 @@ class Cli
     
   puts "find the right care for you "
   Scraper.pull_page
+ # list_hospitals_2
   list_hospitals
   info
   goodbye
-end 
+end
+#def list_hospitals_2
+#run = Hospital.all.sort_by.with_index(1){|hosp| hosp.name}
+ #run.each.with_index(1) do |hospital,i|
+#puts "|#{i}: #{ hospital.name}|"
+#binding.pry
+# end 
+# end 
 def list_hospitals
   
 @ranking = Hospital.all.each.with_index(1) do |hospital,i|
@@ -15,6 +23,7 @@ def list_hospitals
   puts "<--------------------------------------->"
 end
 puts "find out more about the Hospital by seleting a number :"
+
 end    
 
 def info 
